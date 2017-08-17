cd2e02501c54a8c208ebe8e09641860d03749199
Raghu Betina <raghu@firstdraft.com>
2017-08-17 23:14:02 +0000

    Whitelist columns in Entertainment for ActiveAdmin

--- app/admin/entertainment.rb
+++ app/admin/entertainment.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Entertainment do
 
+ permit_params :picture, :title, :category, :rating
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
