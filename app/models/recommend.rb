8348de65e29f78063a6901dac7d4b0994b14f2e1
Raghu Betina <raghu@firstdraft.com>
2017-08-17 23:13:35 +0000

    Direct association between users and recommends

--- app/models/recommend.rb
+++ app/models/recommend.rb
@@ -1,6 +1,9 @@
 class Recommend < ApplicationRecord
   # Direct associations
 
+  belongs_to :receiver,
+             :class_name => "User"
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :recommends,
+             :foreign_key => "receiver_id",
+             :dependent => :destroy
+
   has_many   :ratings,
              :dependent => :destroy
 
